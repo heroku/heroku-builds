327416ba16173557ed7fb02216536103f44f4d40
Cyril David <cyril@heroku.com>
2015-06-15 14:34:09 -0700

    Use a named function instead via @hunterloftis

--- commands/builds/output.js
+++ commands/builds/output.js
@@ -11,23 +11,25 @@ module.exports = {
   description: 'show build output',
   help: 'Show build output for a Heroku app',
   variableArgs: true,
-  run: cli.command(function (context, heroku) {
-    let id = context.args[0];
-
-    if (!id) {
-      cli.error("Usage: heroku builds:output <id> -a <app>")
-      return
-    }
-
-    return heroku.request({
-      path: `/apps/${context.app}/builds/${id}`,
-      method: 'GET',
-      headers: {
-        'Accept': 'application/vnd.heroku+json; version=3.streaming-build-output'
-      },
-      parseJSON: true
-    }).then(function (build) {
-      request(build.output_stream_url).pipe(process.stdout);
-    });
-  })
+  run: cli.command(showOutput)
 };
+
+function showOutput(context, heroku) {
+  let id = context.args[0];
+
+  if (!id) {
+    cli.error("Usage: heroku builds:output <id> -a <app>")
+    return
+  }
+
+  return heroku.request({
+    path: `/apps/${context.app}/builds/${id}`,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/vnd.heroku+json; version=3.streaming-build-output'
+    },
+    parseJSON: true
+  }).then(function (build) {
+    request(build.output_stream_url).pipe(process.stdout);
+  });
+}
