3b1a5ad473f78f17602b9119374b8985b236c9b8
dmathieu <42@dmathieu.com>
2017-04-10 11:56:46 +0200

    make standard syntax happy

--- commands/builds/index.js
+++ commands/builds/index.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-let cli       = require('heroku-cli-util');
-let columnify = require('columnify');
+let cli = require('heroku-cli-util')
+let columnify = require('columnify')
 
 module.exports = {
   topic: 'builds',
@@ -11,28 +11,28 @@ module.exports = {
   help: 'List builds for a Heroku app',
   run: cli.command(function (context, heroku) {
     return heroku.request({
-       path: `/apps/${context.app}/builds`,
-       method: 'GET',
-       headers: {
+      path: `/apps/${context.app}/builds`,
+      method: 'GET',
+      headers: {
         'Range': 'created_at ..; order=desc;'
       },
       parseJSON: true
     }).then(function (builds) {
-      var columnData = builds.slice(0, 10).map(function(build) {
+      var columnData = builds.slice(0, 10).map(function (build) {
         return { created_at: build.created_at,
           status: build.status,
           id: build.id,
           version: build.source_blob.version,
           user: build.user.email
-        };
-      });
+        }
+      })
 
       // TODO: use `max` directive in query to avoid the slice nonsense
       // heroku-client currently breaks if one tries to do this
       var columns = columnify(columnData, {
         showHeaders: false
-      });
-      console.log(columns);
-    });
+      })
+      console.log(columns)
+    })
   })
-};
+}
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 exports.topic = {
   name: 'builds',
   description: 'manage builds'
-};
+}
 
 exports.commands = [
   require('./commands/builds/create'),
   require('./commands/builds/index'),
   require('./commands/builds/output')
-];
+]
